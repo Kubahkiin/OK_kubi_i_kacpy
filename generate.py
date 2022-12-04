79de7d495d43b6c82497acdac940eece37b890b0
kubah <kubahmail@gmail.com>
2022-12-04 22:48:31 +0100

    zatwierdzaaaam

--- generate.py
+++ generate.py
@@ -1,5 +1,12 @@
 import random
 nucs = ['A', 'C', 'G', 'T']
+
+def save_to(filename ,to_save):
+    f1 = open(filename, 'w')
+    for stuff in to_save:
+        f1.writelines(stuff + '\n')
+    f1.close()
+
 def dna(n):
     return ''.join(random.choices(nucs, k=n))
 
@@ -12,19 +19,30 @@ def spectrum(seq, k, n):
     return spectrum
 
 def negative_errors(spectrum, error_count):
+    removed_oligos = []  # storing oligonucleotides removed from spectrum
 
     for i in range(error_count):
-        spectrum.remove(random.choice(spectrum))
+        removed_oligo = random.choice(spectrum)
+        removed_oligos.append(removed_oligo)
+        spectrum.remove(removed_oligo)
+
+    print(f'Negative: {removed_oligos}')
+    save_to('negative_errors.txt', removed_oligos)
 
     return spectrum
 
 
 def positive_errors(spectrum, error_count, k):
+    added_oligos = []  # storing oligonucleotides added to spectrum
 
     for i in range(error_count):
-        while random_k_mer := random.choices(nucs, k=k):
+        while random_k_mer := random.choices(nucs, k=k):  # actually a valid operator that allows for assignment of variables within expressions
             if not spectrum.count(random_k_mer):
                 break
+        added_oligos.append(''.join(random_k_mer))
         spectrum.append(''.join(random_k_mer))
+
+    print(f'Positive: {added_oligos}')
+    save_to('positive_errors.txt', added_oligos)
 
     return spectrum
--- main.py
+++ main.py
@@ -2,19 +2,24 @@ import generate
 
 #parametry
 n = 20
-k = 4
-ne = 0
+k = 3
+ne = 2
 pe = 2
 #
 
 seq = generate.dna(n)
 spectrum = generate.spectrum(seq, k, n)
 print(''.join(seq))
-# sortowanie w celu rozpierdolenia sekwnecji
+# sortowanie w celu pomieszania sekwnecji
+start_oligo = spectrum[0]
+fake_list = []  # save_to() operates on lists so we fool it with a fake one to save one element
+fake_list.append(start_oligo)
+generate.save_to('start.txt', fake_list)
 spectrum.sort()
+print(f'Oligonukleotyd startowy: {start_oligo}')
 print(spectrum)
 # dodanie błędów
-
+# błędy negatywne
 generate.negative_errors(spectrum, ne)
 generate.positive_errors(spectrum, pe, k)
 spectrum.sort()
